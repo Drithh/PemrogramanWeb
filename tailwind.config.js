3406713c0c9aad1673ed3dfffe000df4526230c2
Drithh <adrielalfeus@gmail.com>
2022-03-24 17:04:02 +0700

    hover animation home page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}', '*.html'],
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
+  theme: {
+    fontFamily: {
+      gt: ['GTAmericaExpended', 'sans-serif'],
+      sans: ['BasicCommercial', 'sans-serif'],
+      serif: ['NeueWorldCondensed', 'serif'],
+    },
+    extend: {
+      colors: {
+        dark: '#1f1f1f',
+        light: '#fff',
+        decor: '#f9f9',
+        soft: '#ebe6d7',
+        lineColor: '#d1c7a3',
+        secondary: '#645832',
+      },
+    },
+  },
+  plugins: [],
+};
